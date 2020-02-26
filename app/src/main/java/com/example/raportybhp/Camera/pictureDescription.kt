d7f3b3afa0f64d37e2367f9e1e427e14fd2ed690
micha <bagrowski.michal@gmail.com>
2020-02-26 22:09:50 +0100

    delete image after sending to Firebase

--- app/src/main/java/com/example/raportybhp/Camera/pictureDescription.kt
+++ app/src/main/java/com/example/raportybhp/Camera/pictureDescription.kt
@@ -1,82 +1,99 @@
 package com.example.raportybhp.Camera
 
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.raportybhp.R
+import com.google.android.gms.tasks.Task
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.squareup.picasso.Picasso
+import java.io.File
 
-abstract class pictureDescription : AppCompatActivity() {
+class pictureDescription : AppCompatActivity() {
 
     lateinit var pickBTN: Button
     lateinit var dspBTN: Button
     lateinit var imageToPick: ImageView
     lateinit var picDES : EditText
-//    private lateinit var mStorageRef : StorageReference
-
+    lateinit var mStorageRef : StorageReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.picture_description)
-//        mStorageRef = FirebaseStorage.getInstance().getReference("Images")
 
+        mStorageRef = FirebaseStorage.getInstance().getReference("Images")
 
         imageToPick = findViewById(R.id.imageToPick)
         dspBTN = findViewById(R.id.dspBTN)
-
         pickBTN = findViewById(R.id.nextBTN)
+        picDES = findViewById(R.id.picDES)
 
         dspBTN.setOnClickListener() {
             display()
-//            imageUploader()
+            imageUploader()
         }
 
         pickBTN.setOnClickListener() {
             next()
         }
-
-        picDES = findViewById(R.id.picDES)
-
     }
 
-//    private fun imageUploader() {
-//        var ref = mStorageRef.child(System.currentTimeMillis().toString() + "." + "png")
-//
-//        var filePath = getFile()
-//
-//        val file = Uri.fromFile(filePath)
-//
-//        ref.putFile(file)
-//            .addOnSuccessListener { taskSnapshot ->
-//                // Get a URL to the uploaded content
-//             //   val downloadUrl = taskSnapshot.getDownloadUrl()
-//
-//            Toast.makeText(this, "Image Uploaded Successfully", Toast.LENGTH_SHORT ).show()
-//            }
-//            .addOnFailureListener {
-//                // Handle unsuccessful uploads
-//                // ...
-//            }
-//    }
+
+
+    private fun imageUploader() {
+        var ref = mStorageRef.child(System.currentTimeMillis().toString() + "." + "png")
+
+        var filePath = getFile()
+
+        val file = Uri.fromFile(filePath)
+
+        ref.putFile(file)
+            .addOnSuccessListener { taskSnapshot ->
+              //   Get a URL to the uploaded content
+                val downloadUrl : Task<Uri> = taskSnapshot.storage.downloadUrl
+
+            Toast.makeText(this, "Image Uploaded Successfully", Toast.LENGTH_SHORT ).show()
+                deleteFile()
+            }
+            .addOnFailureListener {
+                // Handle unsuccessful uploads
+                // ...
+            }
+    }
 
     private fun next() {
         var text = picDES.text.toString()
     }
 
-//    private fun getFile() : File {
-//        val extra = intent.getStringExtra("pictureDest")
-//
-//        val sd = Environment.getExternalStorageDirectory()
-//        val dest = File(sd, extra)
-//
-//        return dest
-//    }
+    private fun getFile() : File {
+        val extra = intent.getStringExtra("pictureDest")
+
+        val sd = Environment.getExternalStorageDirectory()
+        val dest = File(sd, extra)
+
+        return dest
+    }
+
+    private fun deleteFile(){
+        val extra = intent.getStringExtra("pictureDest")
+
+        val sd = Environment.getExternalStorageDirectory()
+        val dest = File(sd, extra)
+        dest.delete()
+
+    }
 
     private fun display() {
 
-//        var file = getFile()
+        var file = getFile()
 
-//        Picasso.get().load(file).into(imageToPick)
+        Picasso.get().load(file).into(imageToPick)
     }
+
 }
